1c5c968541d8e918a0e9bf5e499d406b8f057a79
Johan Javier Hernández Núñez <132857486+jjhn11@users.noreply.github.com>
2025-03-19 22:15:43 -0700

    sasssss

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,4 @@
-import './assets/main.css'
+import './assets/scss/main.scss'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -6,7 +6,6 @@ import router from './router'
 import store from './store'
 
 // Import Bootstrap CSS and JS
-import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
